1acdded462aba746228bd71f33412fa65f2481f7
Katsumasa Sato <kaktsumasa-sato@outlook.com>
2023-02-10 22:56:12 +0900

    関連ページを追加

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -12,30 +12,10 @@
 
     <?php if (!get_post_meta($post->ID, 'progression_studios_disable_advertisement_post', true)) : ?>
       <?php if (is_active_sidebar('progression-studios-post-widgets-top')) { ?>
-        <div class="widget-area-top-of-posts"><?php dynamic_sidebar('progression-studios-post-widgets-top'); ?></div>
+        <div class="widget-area-top-of-posts">
+          <?php dynamic_sidebar('progression-studios-post-widgets-top'); ?>
+        </div>
       <?php } ?>
-    <?php endif; ?>
-
-    <?php if (has_post_format('gallery') && get_post_meta($post->ID, 'progression_studios_gallery', true)) : ?>
-      <div class="progression-studios-gallery-post-image">
-        <div class="flexslider progression-studios-gallery">
-          <ul class="slides">
-            <?php $files = get_post_meta(get_the_ID(), 'progression_studios_gallery', 1); ?>
-            <?php foreach ((array) $files as $attachment_id => $attachment_url) : ?>
-              <?php $lightbox_pro = wp_get_attachment_image_src($attachment_id, 'large'); ?>
-              <li>
-                <?php $get_description = get_post($attachment_id)->post_excerpt; ?>
-                <a href="<?php echo esc_attr($lightbox_pro[0]); ?>" data-rel="prettyPhoto[gallery]" <?php if (!empty($get_description)) {
-                                                                                                      echo 'title="' . $get_description . '"';
-                                                                                                    } ?>>
-                  <?php echo wp_get_attachment_image($attachment_id, 'progression-studios-blog-index'); ?>
-                </a>
-              </li>
-            <?php endforeach;  ?>
-          </ul>
-        </div><!-- close .flexslider -->
-
-      </div><!-- close .progression-studios-feaured-image -->
     <?php endif; ?>
 
     <div class="progression-blog-single-content">
@@ -61,11 +41,13 @@
       $summaryGroup = get_field('acf_summary_group');
       if (isset($summaryGroup['acf_summary_text'])) :
       ?>
-      <p>あらすじ</p>
-      <blockquote>
-        <p><?php echo $summaryGroup['acf_summary_text'] ?></p>
-        <cite><a href="<?php echo $summaryGroup['acf_ref_url'] ?>" target="_blank"><?php echo $summaryGroup['acf_summary_ref'] ?></a></cite>
-      </blockquote>
+        <p>あらすじ</p>
+        <blockquote>
+          <p>
+            <?php echo $summaryGroup['acf_summary_text'] ?>
+          </p>
+          <cite><a href="<?php echo $summaryGroup['acf_ref_url'] ?>" target="_blank"><?php echo $summaryGroup['acf_summary_ref'] ?></a></cite>
+        </blockquote>
       <?php endif; ?>
 
       <?php the_content(); ?>
@@ -90,11 +72,13 @@
         $anikore,
       );
 
-      function getIndx($siteName) {
+      function getIndx($siteName)
+      {
         return array_search('score', array_column(get_field_object($siteName)['sub_fields'], 'label'));
       }
 
-      function getMax($siteName) {
+      function getMax($siteName)
+      {
         $indexKey = getIndx($siteName);
         return array("max" => get_field_object($siteName)['sub_fields'][$indexKey]["max"]);
       }
@@ -128,7 +112,9 @@
             <?php endforeach; ?>
           </ul>
           <p class="progression-blog-single-review-site-caution">
-            <strong>本ページの情報は<time datetime="<?php the_time(get_the_date('Y-m-d')); ?>"></time> <?php echo get_the_date('Y年n月j日'); ?>時点のものです。<br>各サイトの最新スコアは各々のサイトにてご確認ください。</strong>
+            <strong>本ページの情報は<time datetime="<?php the_time(get_the_date('Y-m-d')); ?>"></time>
+              <?php echo get_the_date('Y年n月j日'); ?>時点のものです。<br>各サイトの最新スコアは各々のサイトにてご確認ください。
+            </strong>
           </p>
         </div>
       <?php endif; ?>
@@ -141,50 +127,64 @@
           $vodServiceUrl = get_post_meta($post->ID, 'vod_group_service_item_url', true);
           $vodUrl = get_term_link(get_the_terms($post->ID, 'vod')[0]->term_id)
           ?>
-          このページでは<?php echo $vodServiceName; ?>で配信中の<?php echo $titleJp; ?>から執筆しました
+          このページでは
+          <?php echo $vodServiceName; ?>で配信中の
+          <?php echo $titleJp; ?>から執筆しました
         </h2>
         <p class="progression-blog-single-vod-text">
-          <?php echo $vodServiceName ?>で配信されている『<?php echo $titleJp; ?>』のあらすじ、感想、評価を紹介しました。気になる方は、ぜひ下記URLの<?php echo $vodServiceName ?>からチェックしてみてください！
+          <?php echo $vodServiceName ?>で配信されている『
+          <?php echo $titleJp; ?>』のあらすじ、感想、評価を紹介しました。気になる方は、ぜひ下記URLの
+          <?php echo $vodServiceName ?>からチェックしてみてください！
         </p>
         <a href="<?php echo esc_url($vodServiceUrl); ?>" target="_blank" rel="noopener"><?php echo $vodServiceName . ' ' . $titleJp; ?></a>
         <p class="progression-blog-single-vod-caution">
-          <strong>本ページの情報は<time datetime="<?php the_time(get_the_date('Y-m-d')); ?>"></time> <?php echo get_the_date('Y年n月j日'); ?>時点のものです。<br>最新の配信状況は <?php echo $vodServiceName; ?>サイトにてご確認ください。</strong>
+          <strong>本ページの情報は<time datetime="<?php the_time(get_the_date('Y-m-d')); ?>"></time>
+            <?php echo get_the_date('Y年n月j日'); ?>時点のものです。<br>最新の配信状況は
+            <?php echo $vodServiceName; ?>サイトにてご確認ください。
+          </strong>
         </p>
         <p><a href="<?php echo esc_url($vodUrl); ?>">他にも<?php echo $vodServiceName ?>の作品はこちらからご覧いただけます。</a></p>
       </div>
       <div class="clearfix-pro"></div>
     </div><!-- close .progression-blog-single-content -->
 
-    <?php if (get_post_meta($post->ID, 'progression_studios_relation', true)) : ?>
+    <?php
+    $relation_filed = get_field('relation_filed');
+    if ($relation_filed) :
+    ?>
       <div class="progression-blog-single-relation">
         <h2 class="progression-blog-single-relation-heading">関連ページ</h2>
         <ul class="progression-blog-single-relation-lists">
-          <?php
-          $relation = get_post_meta($post->ID, 'progression_studios_relation', true);
-          foreach ((array) $relation as $key => $value) :
-            $relationPostId = (int) $value['progression_studios_relation_post_id'];
-            $relatedCategories = get_the_category($relationPostId);
-            $imageThumbnail = wp_get_attachment_image_src(get_post_thumbnail_id($relationPostId), 'progression-studios-blog-background');
-            $imageUrl = get_post_meta($relationPostId, 'progression_studios_gallery-image-url', true);
-            $image = $imageUrl ? $imageUrl : $imageThumbnail[0];
+          <?php foreach ((array) $relation_filed as $relation_post) :
+            $relation_post_id = (int) $relation_post['release_post_id'];
+            $relation_categories = get_the_category($relation_post_id);
+            $image_thumbnail = wp_get_attachment_image_src(get_post_thumbnail_id($relation_post_id), 'progression-studios-blog-background');
+            $image_url = get_post_meta($relation_post_id, 'progression_studios_gallery-image-url', true);
+            $image = $image_url ? $image_url : $image_thumbnail[0];
           ?>
-            <li class="progression-blog-single-relation-list" style="<?php if ($image) { echo "background-image: url(" . esc_url($image) . ")"; } ?>">
-              <a href="<?php echo get_permalink($relationPostId); ?>">
-                <?php if (get_the_category($relationPostId)) : ?>
+            <li class="progression-blog-single-relation-list" style="<?php echo "background-image: url(" . esc_url($image) . ")"; ?>">
+              <a href="<?php echo get_permalink($relation_post_id); ?>">
+                <?php if (get_the_category($relation_post_id)) : ?>
                   <ul class="progression-blog-single-relation-category-lists">
-                    <?php foreach ((array) $relatedCategories as $relatedCategory) : ?>
+                    <?php foreach ((array) $relation_categories as $relation_category) : ?>
                       <li class="progression-blog-single-relation-category-list">
-                        <?php echo $relatedCategory->name; ?>
+                        <?php echo $relation_category->name; ?>
                       </li>
                     <?php endforeach; ?>
                   </ul>
                 <?php endif; ?>
                 <div class="progression-blog-single-relation-content">
                   <div class="progression-blog-single-relation-score">
-                    <div class="progression-blog-single-relation-score-value"><?php echo get_post_meta($relationPostId, 'review_score', true); ?></div>
+                    <div class="progression-blog-single-relation-score-value">
+                      <?php echo get_post_meta($relation_post_id, 'review_score', true); ?>
+                    </div>
                   </div>
-                  <div class="progression-blog-single-relation-title"><?php echo get_the_title($relationPostId); ?></div>
-                  <div class="progression-blog-single-relation-date"><?php echo get_the_date('Y/m/d', $relationPostId); ?></div>
+                  <div class="progression-blog-single-relation-title">
+                    <?php echo get_the_title($relation_post_id); ?>
+                  </div>
+                  <div class="progression-blog-single-relation-date">
+                    <?php echo get_the_date('Y/m/d', $relation_post_id); ?>
+                  </div>
                 </div>
               </a>
             </li>
@@ -194,18 +194,20 @@
     <?php endif; ?>
 
     <?
-      // 劇場版以外レンタルサービスを表示
-      if ($vodServiceSlug !== 'theater') {
-        get_template_part('template-parts/content-ad-rental');
-      }
+    // 劇場版以外レンタルサービスを表示
+    if ($vodServiceSlug !== 'theater') {
+      get_template_part('template-parts/content-ad-rental');
+    }
     ?>
 
-<?php wp_link_pages(array(
-      'before' => '<div class="progression-page-nav">' . esc_html__('Pages:', 'ratency-progression'),
-      'after'  => '</div>',
-      'link_before' => '<span>',
-      'link_after'  => '</span>',
-    ));
+    <?php wp_link_pages(
+      array(
+        'before' => '<div class="progression-page-nav">' . esc_html__('Pages:', 'ratency-progression'),
+        'after' => '</div>',
+        'link_before' => '<span>',
+        'link_after' => '</span>',
+      )
+    );
     ?>
     <div class="clearfix-pro"></div>
 
@@ -216,7 +218,9 @@
 
     <?php if (!get_post_meta($post->ID, 'progression_studios_disable_advertisement_post', true)) : ?>
       <?php if (is_active_sidebar('progression-studios-post-widgets-bottom')) { ?>
-        <div class="widget-area-bottom-of-posts"><?php dynamic_sidebar('progression-studios-post-widgets-bottom'); ?></div>
+        <div class="widget-area-bottom-of-posts">
+          <?php dynamic_sidebar('progression-studios-post-widgets-bottom'); ?>
+        </div>
       <?php } ?>
     <?php endif; ?>
 
@@ -225,11 +229,15 @@
     <?php the_tags('<div class="tags-progression"><span><i class="fa fa-tags"></i></span>', '', '</div><div class="clearfix-pro"></div>'); ?>
 
 
-    <?php if (get_theme_mod('progression_studios_blog_post_related') == 'true') : ?><?php get_template_part('template-parts/related', 'posts'); ?><?php endif; ?>
+    <?php if (get_theme_mod('progression_studios_blog_post_related') == 'true') : ?>
+      <?php get_template_part('template-parts/related', 'posts'); ?>
+    <?php endif; ?>
 
 
-    <?php if (comments_open() || get_comments_number()) : comments_template();
-    endif; ?><div class="clearfix-pro"></div>
+    <?php if (comments_open() || get_comments_number()) :
+      comments_template();
+    endif; ?>
+    <div class="clearfix-pro"></div>
 
   </div><!-- close .progression-single-container -->
 </div><!-- #post-## -->
