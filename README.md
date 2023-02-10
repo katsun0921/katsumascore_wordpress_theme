66b71de6338614631c7415a97b74a4f0b0f4ef93
Katsumasa Sato <kaktsumasa-sato@outlook.com>
2023-02-10 23:18:42 +0900

    おすすめポイントを追加

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # katsumascore_wordpress_theme
 katsumascore.blog wordpress theme
+
+phpの変数名はスネークケースとする。※キャメルケースだと反映できないことがある。
+
+$snake_case
--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -19,19 +19,25 @@
     <?php endif; ?>
 
     <div class="progression-blog-single-content">
-      <?php if (get_post_meta($post->ID, 'progression_studios_good_point', true)) : ?>
+      <?php
+      $good_points_filed = get_field('good_point_filed');
+      if ($good_points_filed) :
+      ?>
         <div class="progression-blog-single-good-point-container">
           <h2 class="progression-blog-single-good-point">ここがおすすめ！</h2>
           <ul class="progression-blog-single-good-point-lists">
             <?php
-            $goodPoints = get_post_meta($post->ID, 'progression_studios_good_point', true);
-            foreach ((array) $goodPoints as $key => $value) :
-              $goodPointText = (string) $value['progression_studios_good_point_text'];
+            foreach ((array) $good_points_filed as $good_point) :
+              $good_point_text = (string) $good_point['good_point_text'];
+              if ($good_point_text) :
             ?>
-              <li class="progression-blog-single-good-point-list">
-                <?php echo $goodPointText ?>
-              </li>
-            <?php endforeach; ?>
+                <li class="progression-blog-single-good-point-list">
+                  <?php echo $good_point_text ?>
+                </li>
+            <?php
+              endif;
+            endforeach;
+            ?>
           </ul>
         </div>
       <?php endif; ?>
