4158472e6e27408a6beacde89385585da6e82b59
Katsumasa Sato <kaktsumasa-sato@outlook.com>
2023-03-16 10:39:36 +0900

    Fix preloader of js

--- footer.php
+++ footer.php
@@ -11,30 +11,30 @@
 ?>
 
 <?php if (get_theme_mod('progression_studios_footer_elementor_library') && !is_singular('elementor_library')) : ?>
-  <div id="progression-studios-footer-page-builder">
-    <?php if (is_page() && get_post_meta($post->ID, 'progression_studios_disable_footer_per_page', true)) : ?><?php else : ?>
-    <?php
+<div id="progression-studios-footer-page-builder">
+  <?php if (is_page() && get_post_meta($post->ID, 'progression_studios_disable_footer_per_page', true)) : ?><?php else : ?>
+  <?php
                                                                                                               if (function_exists('elementor_load_plugin_textdomain')) {
                                                                                                                 $progression_studios_elementor_footer_instance = Elementor\Plugin::instance();
                                                                                                                 echo  $progression_studios_elementor_footer_instance->frontend->get_builder_content_for_display(get_theme_mod('progression_studios_footer_elementor_library'));
                                                                                                               }
     ?><?php endif; ?>
-  </div>
+</div>
 <?php else : ?>
-  <footer id="site-footer" class="progression-studios-footer-normal-width <?php echo esc_attr(get_theme_mod('progression_studios_footer_copyright_location', 'footer-copyright-align-left')); ?>">
-    <div id="progression-studios-copyright">
-      <div id="copyright-divider-top"></div>
-      <div class="l-container">
-        <div id="copyright-text">
-          <?php echo wp_kses(get_theme_mod('progression_studios_footer_copyright', 'All Rights Reserved. Developed by Progression Studios'), true); ?>
-        </div>
-      </div> <!-- close .l-container -->
-      <div class="clearfix-pro"></div>
-    </div><!-- close #progression-studios-copyright -->
-  </footer>
+<footer id="site-footer"
+  class="progression-studios-footer-normal-width <?php echo esc_attr(get_theme_mod('progression_studios_footer_copyright_location', 'footer-copyright-align-left')); ?>">
+  <div id="progression-studios-copyright">
+    <div id="copyright-divider-top"></div>
+    <div class="l-container">
+      <div id="copyright-text">
+        <?php echo wp_kses(get_theme_mod('progression_studios_footer_copyright', 'All Rights Reserved. Developed by Progression Studios'), true); ?>
+      </div>
+    </div> <!-- close .l-container -->
+    <div class="clearfix-pro"></div>
+  </div><!-- close #progression-studios-copyright -->
+</footer>
 <?php endif; ?>
 
-</div><!-- close #boxed-layout-pro -->
 <a href="#0" id="pro-scroll-top"><?php esc_html_e('Scroll to top', 'ratency-progression'); ?></a>
 
 <?php wp_footer(); ?>
