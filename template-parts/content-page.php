a01b0f5ac25397f9ccd27addc8077f13a5d9cec3
Katsumasa Sato <kaktsumasa-sato@outlook.com>
2023-03-31 13:56:28 +0900

    fix: pagination style

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template used for displaying page content in page.php
  *
@@ -6,12 +7,12 @@
  */
 ?>
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <?php the_content(); ?>
-  <?php wp_link_pages( array(
-					'before' => '<div class="progression-page-nav">' . esc_html__( 'Pages:', 'ratency-progression' ),
-					'after'  => '</div>',
-					'link_before' => '<span>',
-					'link_after'  => '</span>',
-				) );
-			?>
+	<?php the_content(); ?>
+	<?php wp_link_pages(array(
+		'before' => '<div class="c-pagination">' . esc_html__('Pages:', 'ratency-progression'),
+		'after'  => '</div>',
+		'link_before' => '<span>',
+		'link_after'  => '</span>',
+	));
+	?>
 </div>
