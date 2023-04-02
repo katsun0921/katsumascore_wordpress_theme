900a776d796eb9e2d22f3040bcc354c4dabc8363
Katsumasa Sato <kaktsumasa-sato@outlook.com>
2023-04-02 23:55:14 +0900

    fix: class name

--- template-parts/content-overlay.php
+++ template-parts/content-overlay.php
@@ -4,21 +4,21 @@ $image = $imageThumbnail[0];
 ?>
 
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <div class="l-post__overlay">
+  <div class="l-postOverlay">
     <a href="<?php the_permalink(); ?>" class="u-relative">
       <?php if ('post' == get_post_type()) : ?>
-      <div class="c-category c-category__small u-z-20"><?php the_category(' '); ?></div>
+        <div class="c-category c-category__small u-z-20"><?php the_category(' '); ?></div>
       <?php endif; ?>
       <?php if (has_post_thumbnail()) : ?>
-      <div class="l-post__overlayImage" style="background-image:url('<?php echo esc_attr($image); ?>')"></div>
+        <div class="l-postOverlay__image" style="background-image:url('<?php echo esc_attr($image); ?>')"></div>
       <?php endif; ?>
 
       <?php if (get_post_meta($post->ID, 'review_score', true)) : ?>
-      <span class="c-score u-z-20 u-absolute u-right-1-5 u-top-1-5">
-        <span class="c-score__count">
-          <?php echo esc_attr(get_post_meta($post->ID, 'review_score', true)); ?>
+        <span class="c-score u-z-20 u-absolute u-right-1-5 u-top-1-5">
+          <span class="c-score__count">
+            <?php echo esc_attr(get_post_meta($post->ID, 'review_score', true)); ?>
+          </span>
         </span>
-      </span>
       <?php endif; ?>
       <h3 class="c-heading__post"><?php the_title(); ?></h3>
     </a>
--- template-parts/content-related.php
+++ template-parts/content-related.php
@@ -4,24 +4,24 @@ $image = $imageThumbnail[0];
 ?>
 
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <div class="l-post__overlay">
+  <div class="l-postOverlay">
     <a href="<?php the_permalink(); ?>">
       <?php if (has_post_thumbnail()) : ?>
-      <div class="l-post__overlayImage" style="background-image:url('<?php echo esc_attr($image); ?>')"></div>
+        <div class="l-postOverlay__image" style="background-image:url('<?php echo esc_attr($image); ?>')"></div>
       <?php endif; ?>
 
       <?php if (get_post_meta($post->ID, 'review_score', true)) : ?>
-      <span class="c-score u-z-20 u-absolute u-right-1-5 u-top-1-5">
-        <span class="c-score__count">
-          <?php echo esc_attr(get_post_meta($post->ID, 'review_score', true)); ?>
+        <span class="c-score u-z-20 u-absolute u-right-1-5 u-top-1-5">
+          <span class="c-score__count">
+            <?php echo esc_attr(get_post_meta($post->ID, 'review_score', true)); ?>
+          </span>
         </span>
-      </span>
       <?php endif; ?>
 
       <div class="c-category">
         <?php foreach ((get_the_category()) as $category) {
-                echo '<span>' . $category->cat_name . '</span>';
-              } ?>
+          echo '<span>' . $category->cat_name . '</span>';
+        } ?>
       </div>
       <h3 class="c-heading__post"><?php the_title(); ?></h3>
     </a>
