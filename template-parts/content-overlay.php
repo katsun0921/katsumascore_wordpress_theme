f71f708290913c98813814e631036d1963b72a3e
Katsumasa Sato <kaktsumasa-sato@outlook.com>
2023-04-04 10:13:52 +0900

    fix: class name

--- template-parts/content-overlay.php
+++ template-parts/content-overlay.php
@@ -6,14 +6,14 @@ $cat = $cats[0];
 ?>
 
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <a href="<?php the_permalink(); ?>" class="l-postOverlay">
+  <a href="<?php the_permalink(); ?>" class="l-postImageOverlay">
     <?php if ('post' == get_post_type()) : ?>
     <div class="u-z-20 u-relative">
       <div class="c-category c-category__small u-z-20"><?php echo $cat->cat_name; ?></div>
     </div>
     <?php endif; ?>
     <?php if (has_post_thumbnail()) : ?>
-      <div class="l-postOverlay__image" style="background-image:url('<?php echo esc_attr($image); ?>')"></div>
+      <div class="l-postImageOverlay__image" style="background-image:url('<?php echo esc_attr($image); ?>')"></div>
     <?php endif; ?>
 
     <?php if (get_post_meta($post->ID, 'review_score', true)) : ?>
@@ -25,7 +25,7 @@ $cat = $cats[0];
       </span>
       </div>
     <?php endif; ?>
-    <div class="l-postOverlay__content">
+    <div class="l-postImageOverlay__content">
       <h3 class="c-heading__post"><?php the_title(); ?></h3>
     </div>
   </a>
--- template-parts/content-related.php
+++ template-parts/content-related.php
@@ -4,9 +4,9 @@ $image = $imageThumbnail[0];
 ?>
 
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <a href="<?php the_permalink(); ?>" class="l-postOverlay">
+  <a href="<?php the_permalink(); ?>" class="l-postImageOverlay">
       <?php if (has_post_thumbnail()) : ?>
-        <div class="l-postOverlay__image" style="background-image:url('<?php echo esc_attr($image); ?>')"></div>
+        <div class="l-postImageOverlay__image" style="background-image:url('<?php echo esc_attr($image); ?>')"></div>
       <?php endif; ?>
 
       <?php if (get_post_meta($post->ID, 'review_score', true)) : ?>
@@ -25,7 +25,7 @@ $image = $imageThumbnail[0];
           echo '<span>' . $category->cat_name . '</span>';
         } ?>
       </div>
-      <div class="l-postOverlay__content">
+      <div class="l-postImageOverlay__content">
         <h3 class="c-heading__post"><?php the_title(); ?></h3>
       </div>
   </a>
