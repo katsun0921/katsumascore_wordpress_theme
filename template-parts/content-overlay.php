0a5d9ba86128d419c11223beb8238e20fbcb25b0
Katsumasa Sato <kaktsumasa-sato@outlook.com>
2023-05-02 13:42:40 +0900

    fix: mansonry styles

--- template-parts/content-overlay.php
+++ template-parts/content-overlay.php
@@ -1,4 +1,5 @@
 <?php
+$post_id = $post->ID;
 $imageThumbnail = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'progression-studios-blog-background');
 $image = $imageThumbnail[0];
 $cats = get_the_category();
@@ -6,23 +7,17 @@ $cat = $cats[0];
 ?>
 
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <a href="<?php the_permalink(); ?>" class="l-postImageOverlay">
+  <a href="<?php the_permalink(); ?>" class="l-postImageOverlay" <?php if (has_post_thumbnail()) : ?>
+    style="background-image:url('<?php echo esc_attr($image); ?>')" <?php endif; ?>>
     <?php if ('post' == get_post_type()) : ?>
     <div class="u-z-20 u-relative">
       <div class="c-category c-category__small u-z-20"><?php echo $cat->cat_name; ?></div>
     </div>
     <?php endif; ?>
-    <?php if (has_post_thumbnail()) : ?>
-    <div class="l-postImageOverlay__image" style="background-image:url('<?php echo esc_attr($image); ?>')"></div>
-    <?php endif; ?>
 
     <?php if (get_post_meta($post->ID, 'review_score', true)) : ?>
     <div class="u-absolute u-right-1-5 u-top-1-5">
-      <span class="c-score u-z-20">
-        <span class="c-score__count">
-          <?php echo esc_attr(get_post_meta($post->ID, 'review_score', true)); ?>
-        </span>
-      </span>
+      <?php get_template_part('template-parts/components/Score', null, array('post_id' => $post_id)); ?>
     </div>
     <?php endif; ?>
     <div class="l-postImageOverlay__content">
