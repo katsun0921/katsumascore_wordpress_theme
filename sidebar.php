d27acf44d2f2b7ec4f0ed803ed4ee5b3a4558cbd
Katsumasa Sato <kaktsumasa-sato@outlook.com>
2023-05-22 08:11:02 +0900

    Add: admax script

--- sidebar.php
+++ sidebar.php
@@ -10,4 +10,7 @@
 
 <div class="l-sidebar">
   <?php dynamic_sidebar('progression-studios-post-widget-sidebar'); ?>
+  <!-- admax -->
+  <script src="https://adm.shinobi.jp/s/208cd790301f9b66114040a80ad2251a"></script>
+  <!-- admax -->
 </div><!-- close .sidebar -->
