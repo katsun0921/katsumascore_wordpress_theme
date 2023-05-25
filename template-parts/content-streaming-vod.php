390ca7acf29f94b7ea5363668935febe996a95b3
Katsumasa Sato <kaktsumasa-sato@outlook.com>
2023-05-25 17:20:54 +0900

    Fix: add en text for streaming temp

--- template-parts/content-streaming-vod.php
+++ template-parts/content-streaming-vod.php
@@ -1,6 +1,7 @@
 <?php
 // コンテンツを配信していVODを表示するページ。劇場で上映中でありVODで配信をしていない時は表示をしない。
-$titleJp = get_post_meta($post->ID, 'title_jp', true);
+$title_jp = get_post_meta($post->ID, 'title_jp', true);
+$original_title = get_post_meta($post->ID, 'original_title', true);
 $repeater_other_vod = 'streaming_vod_other_vod';
 $rows_other_vod = get_field($repeater_other_vod);
 $count_other_vod = $rows_other_vod ? count($rows_other_vod) : 0;
@@ -9,7 +10,7 @@ $count_other_vod = $rows_other_vod ? count($rows_other_vod) : 0;
 <?php if ($count_other_vod > 0) : ?>
 <section>
   <h2>
-    <?php echo $titleJp ?>は他にも動画配信サービスで配信中です。
+    <?php echo pll_current_language() === 'en' ? 'You can access ' . $original_title . ' through various other video distribution services as well.' : $title_jp . 'は' ?>
   </h2>
   <ul style="flex-wrap: wrap;display: flex;list-style: none;">
     <?php
@@ -21,28 +22,31 @@ $count_other_vod = $rows_other_vod ? count($rows_other_vod) : 0;
         $affiliate_path = 'template-parts/affiliate/';
         $url = $rows_other_vod[$i]['other_vod_url'];
         $slug = get_term($vod_term_id)->slug;
+        $amazon_prime_video = pll_current_language() === 'en' ? 'amazon-prime-video-com' : 'amazon-prime-video-jp';
+        $netflix = pll_current_language() === 'en' ? 'netflix-com' : 'netflix-jp';
+        $disney_plus = pll_current_language() === 'en' ? 'disneyplus-com' : 'disneyplus-jp';
         switch ($slug) {
-          case 'amazon_prime_video':
+          case $amazon_prime_video:
             $affiliate = $affiliate_path . 'amazon-prime-video';
             break;
           case 'abema_tv':
             $affiliate = $affiliate_path . 'abema-tv';
             break;
-          case 'netflix':
+          case $netflix:
             $affiliate = $affiliate_path . 'netflix';
             break;
           case 'u-next':
             $affiliate = $affiliate_path . 'u-next';
             break;
-          case 'disney_plus':
+          case $disney_plus:
             $affiliate = $affiliate_path . 'disney-plus';
             break;
           default:
             $affiliate = null;
         }
         $arg_affiliate = array(
-          'unregistered_text' => '未登録の方はこちらから登録できます。',
-          'streaming_text' => '配信はこちらのリンクから移動できます。',
+          'unregistered_text' => pll_current_language() === 'en' ? 'If you have not yet registered, you can do so here.' : '未登録の方はこちらから登録できます。',
+          'streaming_text' => pll_current_language() === 'en' ? 'You can access the distribution by following this link.' : '配信はこちらのリンクから移動できます。',
           'url' => $url,
           'is_affiliate_code' => $is_affiliate_code,
           'affiliate_code' => $affiliate_code,
