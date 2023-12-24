ad284eb844a5f86b563bae595889b7dc5d287f1b
Katsumasa Sato <kaktsumasa-sato@outlook.com>
2023-12-24 17:39:57 +0900

    fix: add facebook

--- sidebar.php
+++ sidebar.php
@@ -12,5 +12,11 @@
   <?php dynamic_sidebar('progression-studios-post-widget-sidebar'); ?>
   <!-- admax -->
   <script src="https://adm.shinobi.jp/s/208cd790301f9b66114040a80ad2251a"></script>
-  <!-- admax -->
+  <!-- facebook -->
+  <div class="u-w-full u-mt-14">
+    <iframe
+      src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fp%2FKatsumascore-100072246676709%2F&tabs=timeline&width=340&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=177731676219853"
+      width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true"
+      allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+  </div>
 </div><!-- close .sidebar -->
