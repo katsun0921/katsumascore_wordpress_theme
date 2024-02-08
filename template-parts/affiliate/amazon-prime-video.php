fc8236917ef5ce1ff55b31094d0ac6e25b963f6d
Katsumasa Sato <kaktsumasa-sato@outlook.com>
2024-02-08 23:27:04 +0900

    bug: amazon prime video image

--- template-parts/affiliate/amazon-prime-video.php
+++ template-parts/affiliate/amazon-prime-video.php
@@ -2,11 +2,19 @@
 <p style="margin-bottom: 0px;">
   <?php echo $unregistered_text; ?>
 </p>
-<iframe
-  src="https://rcm-fe.amazon-adsystem.com/e/cm?o=9&p=294&l=ur1&category=primevideochannel&banner=0NSPGYVA9YXSZRGEDP02&f=ifr&linkID=9881024e025ba0cf5882037577fb6787&t=katsumascore-22&tracking_id=katsumascore-22"
-  width="320" height="100" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"
-  sandbox="allow-scripts allow-same-origin allow-popups allow-top-navigation-by-user-activation"
-  title="Amazon Prime Video"></iframe>
-<a style="display: block;" href="<?php echo esc_url($url) ?>" target="_blank" rel="noopener noreferrer">
+<div>
+  <a href="<?php echo esc_url($url) ?>">
+    <img
+      src="<?php echo get_template_directory_uri() . '/images/banner/amazon-prime-video.webp' ?>"
+      alt="Amazon Prime Video"
+    >
+  </a>
+</div>
+<a
+  style="display: block;"
+  href="<?php echo esc_url($url) ?>"
+  target="_blank"
+  rel="noopener noreferrer"
+>
   <?php echo $streaming_text; ?>
 </a>
--- template-parts/affiliate/netflix.php
+++ template-parts/affiliate/netflix.php
@@ -3,7 +3,7 @@
   <?php echo $unregistered_text; ?>
 </p>
 <div>
-  <a href="https://www.netflix.com/jp/"><img
+  <a href="<?php echo esc_url($url) ?>"><img
       src="<?php echo get_template_directory_uri() . '/images/banner/netflix.webp' ?>" alt="Netflix"></a>
 </div>
 <a style="display: block;" href="<?php echo esc_url($url) ?>" target="_blank" rel="noopener noreferrer">
