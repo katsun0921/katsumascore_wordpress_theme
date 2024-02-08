f989f16abf74376b7e73bbc5f0e9c26823f352b8
Katsumasa Sato <kaktsumasa-sato@outlook.com>
2024-02-08 22:59:41 +0900

    fix: add apple tv banner

--- template-parts/affiliate/apple-tv-plus.php
+++ template-parts/affiliate/apple-tv-plus.php
@@ -2,11 +2,8 @@
 <p style="margin-bottom: 0px;">
   <?php echo $unregistered_text; ?>
 </p>
-<div>
-  <a href="https://www.disneyplus.com/ja-jp/welcome/stream-now">
-    <img src="<?php echo get_template_directory_uri() . '/images/banner/disney-plus.webp' ?>" alt="Disney Plus"
-      loading="lazy"></a>
-</div>
 <a style="display: block;" href="<?php echo esc_url($url) ?>" target="_blank" rel="noopener noreferrer">
+  <img src="<?php echo get_template_directory_uri() . '/images/banner/apple-tv-plus.webp' ?>" alt="youtube"
+    loading="lazy">
   <?php echo $streaming_text; ?>
 </a>
