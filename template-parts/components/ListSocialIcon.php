ef21032f602997ee841701fb6509bc5ff91b2536
Katsumasa Sato <kaktsumasa-sato@outlook.com>
2024-11-04 13:57:29 +0900

    fix: update x icon

--- template-parts/components/ListSocialIcon.php
+++ template-parts/components/ListSocialIcon.php
@@ -10,24 +10,51 @@ $facebookLink = get_theme_mod('progression_studios_general_facebook');
 <ul class="c-list__socialLink">
   <?php if ($twitterLink) : ?>
   <li>
-    <a href="<?php echo esc_url($twitterLink) ?>" target="_blank" class="c-icon__circle">
-      <img src="https://katsumascore.blog/images/logo-twitter-blue-circle.png" alt="twitter" width="50" height="50"
-        class="u-block" />
+    <a
+      href="<?php echo esc_url($twitterLink) ?>"
+      target="_blank"
+      class="c-icon__circle"
+    >
+      <img
+        src="<?php echo get_template_directory_uri() . '/images/icons/logo-x-white.png' ?>"
+        alt="X"
+        width="50"
+        height="50"
+        class="u-block"
+      />
     </a>
   </li>
   <?php endif ?>
   <?php if ($facebookLink) : ?>
   <li>
-    <a href="<?php echo esc_url($facebookLink) ?>" target="_blank" class="c-icon__circle">
-      <img src="https://katsumascore.blog/images/logo-facebook.png" alt="facebook" width="50" height="50"
-        class="u-block" />
+    <a
+      href="<?php echo esc_url($facebookLink) ?>"
+      target="_blank"
+      class="c-icon__circle"
+    >
+      <img
+        src="<?php echo get_template_directory_uri() . '/images/icons/logo-facebook.png' ?>"
+        alt="facebook"
+        width="50"
+        height="50"
+        class="u-block"
+      />
     </a>
   </li>
   <?php endif ?>
   <?php if ($rssLink) : ?>
   <li>
-    <a href="<?php echo esc_url($rssLink) ?>" class="c-icon__rss c-icon__circle" target="_blank">
-      <img src="https://katsumascore.blog/images/logo-rss-white.png" alt="rss" width="30" height="30" />
+    <a
+      href="<?php echo esc_url($rssLink) ?>"
+      class="c-icon__rss c-icon__circle"
+      target="_blank"
+    >
+      <img
+        src="<?php echo get_template_directory_uri() . '/images/icons/logo-rss-white.png' ?>"
+        alt="rss"
+        width="30"
+        height="30"
+      />
     </a>
   </li>
   <?php endif ?>
--- template-parts/content-sharing.php
+++ template-parts/content-sharing.php
@@ -3,14 +3,21 @@ $sharingTitle = htmlspecialchars(urlencode(html_entity_decode(get_the_title(), E
 ?>
 <ul class="l-sharing">
   <li class="l-sharing__block l-sharing__twitter">
-    <a href="https://twitter.com/share?text=<?php echo $sharingTitle; ?>&url=<?php echo urlencode(the_permalink()); ?>"
-      class="c-link__social c-link__twitter" target="_blank">
-      <span class="c-link__socialText">Share on Twitter</span>
+    <a
+      href="https://x.com/share?text=<?php echo $sharingTitle; ?>&url=<?php echo urlencode(the_permalink()); ?>"
+      class="c-link__social"
+      target="_blank"
+      style="background: #000"
+    >
+      <span class="c-link__socialText">Share on X</span>
     </a>
   </li>
   <li class="l-sharing__block l-sharing__twitter">
-    <a href="http://www.facebook.com/sharer.php?u=<?php echo $sharingTitle; ?>" class="c-link__social c-link__facebook"
-      target="_blank">
+    <a
+      href="http://www.facebook.com/sharer.php?u=<?php echo $sharingTitle; ?>"
+      class="c-link__social c-link__facebook"
+      target="_blank"
+    >
       <span class="c-link__socialText">Share on Facebook</span>
     </a>
   </li>
