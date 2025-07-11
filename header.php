0d8a0e61f991a4e0611af80717b0ae074231f083
佐藤功理 <katsu@MacBook-Air.local>
2025-07-11 21:58:16 +0900

    Create Schema file

--- header.php
+++ header.php
@@ -33,6 +33,7 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php get_template_part('header/social', 'sharing'); ?>
+  <?php get_template_part('header/schema'); ?>
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
   <link rel="preconnect" href="https://fonts.googleapis.com">
